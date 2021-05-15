c25b1d0574b809a4c1fc0b46a4b0e87df1250128
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-15 22:57:32 +0600

    logout edited

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,6 @@
 
 @section('content')
 
-
-
-
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -42,23 +38,13 @@
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </a>
+        <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
   </div>
 </nav>
 
