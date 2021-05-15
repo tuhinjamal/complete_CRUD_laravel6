9a40ceb348e4083f596ee098223bc77ea07f4f89
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-15 20:15:16 +0600

    error of homepage corrected

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -25,6 +25,45 @@
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- Tell the browser to be responsive to screen width -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Font Awesome -->
+      <link rel="stylesheet" href="{{asset('backend/plugins/fontawesome-free/css/all.min.css')}}">
+      <!-- Ionicons -->
+      <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+      <!-- Tempusdominus Bbootstrap 4 -->
+      <link rel="stylesheet" href="{{asset('backend/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+      <!-- iCheck -->
+      <link rel="stylesheet" href="{{asset('backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+      <!-- JQVMap -->
+      <link rel="stylesheet" href="{{asset('backend/plugins/jqvmap/jqvmap.min.css')}}">
+      <!-- Theme style -->
+      <link rel="stylesheet" href="{{asset('backend/dist/css/adminlte.min.css')}}">
+      <!-- overlayScrollbars -->
+      <link rel="stylesheet" href="{{asset('backend/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+      <!-- Daterange picker -->
+      <link rel="stylesheet" href="{{asset('backend/plugins/daterangepicker/daterangepicker.css')}}">
+      <!-- summernote -->
+      <link rel="stylesheet" href="{{asset('backend/plugins/summernote/summernote-bs4.css')}}">
+      <!-- Google Font: Source Sans Pro -->
+      <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+      <!-- DataTables -->
+      <link rel="stylesheet" href="{{asset('backend/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+      <link rel="stylesheet" href="{{asset('backend/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+      <!-- jQuery -->
+        <script src="{{asset('backend/plugins/jquery/jquery.min.js')}}"></script>
+          <style type="text/css">
+            .notifyjs-corner{
+              z-index: 10000 !important
+            }
+          </style>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"></script>
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+        <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
+        <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
 <!------ Include the above in your HEAD tag ---------->
 
   
@@ -46,5 +85,48 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
         <!-- Core theme JS-->
         <script src="{{asset('frontend/js/scripts.js')}}"></script>
+                
+        <!-- jQuery UI 1.11.4 -->
+        <script src="{{asset('backend/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+        <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+        <script>
+          $.widget.bridge('uibutton', $.ui.button)
+        </script>
+        <!-- Bootstrap 4 -->
+        <script src="{{asset('backend/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <!-- ChartJS -->
+        <script src="{{asset('backend/plugins/chart.js/Chart.min.js')}}"></script>
+        <!-- Sparkline -->
+        <script src="{{asset('backend/plugins/sparklines/sparkline.js')}}"></script>
+        <!-- JQVMap -->
+        <script src="{{asset('backend/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+        <!-- jQuery Knob Chart -->
+        <script src="{{asset('backend/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+        <!-- daterangepicker -->
+        <script src="{{asset('backend/plugins/moment/moment.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/daterangepicker/daterangepicker.js')}}"></script>
+        <!-- Tempusdominus Bootstrap 4 -->
+        <script src="{{asset('backend/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+        <!-- Summernote -->
+        <script src="{{asset('backend/plugins/summernote/summernote-bs4.min.js')}}"></script>
+        <!-- overlayScrollbars -->
+        <script src="{{asset('backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+        <!-- AdminLTE App -->
+        <script src="{{asset('backend/dist/js/adminlte.js')}}"></script>
+        <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+        <script src="{{asset('backend/dist/js/pages/dashboard.js')}}"></script>
+        <!-- AdminLTE for demo purposes -->
+        <script src="{{asset('backend/dist/js/demo.js')}}"></script>
+        <!-- DataTables -->
+        <script src="{{asset('backend/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+        <!-- jquery-validation -->
+        <script src="{{asset('backend/plugins/jquery-validation/jquery.validate.min.js')}}"></script>
+        <script src="{{asset('backend/plugins/jquery-validation/additional-methods.min.js')}}"></script>
+
+        <!-- page script -->
     </body>
 </html>
