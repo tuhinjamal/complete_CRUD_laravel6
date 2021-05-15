71af800e853e238fb68db8ee5badb6677dc9382d
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-15 16:35:57 +0600

    login register added

--- resources/views/frontend/layouts/home.blade.php
+++ resources/views/frontend/layouts/home.blade.php
@@ -13,10 +13,17 @@
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Services</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+                      @if (Route::has('login'))
+                        @auth
+                          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ url('/home') }}">home</a></li>
+                          @else
+                          <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ route('login') }}">login</a></li>
+                             @if (Route::has('register'))
+                             <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ route('register') }}">register</a></li>
+                             @endif
+                        @endauth
+                       @endif   
+
                     </ul>
                 </div>
             </div>
