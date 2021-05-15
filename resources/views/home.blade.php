6f96e98eae3361f2b96a930bb6b8c735c20ddca6
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-15 23:00:36 +0600

    logout edited

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,11 @@
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
     <ul class="navbar-nav mr-auto">
     @guest
       <li class="nav-item active">
@@ -41,10 +46,6 @@
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
 </nav>
 
