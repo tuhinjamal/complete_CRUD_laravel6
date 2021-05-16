72608d40cbb2282c60e6be3e408e1f871d4859d0
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-17 00:26:45 +0600

    ledger started to add

--- database/migrations/2021_05_16_182418_create_ledgermodels_table.php
+++ database/migrations/2021_05_16_182418_create_ledgermodels_table.php
@@ -15,6 +15,17 @@ class CreateLedgermodelsTable extends Migration
     {
         Schema::create('ledgermodels', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('cash')->nullable();
+            $table->integer('receivable')->nullable();
+            $table->integer('equipment')->nullable();
+            $table->integer('note_payble')->nullable();
+            $table->integer('account_payble')->nullable();
+            $table->integer('capital')->nullable();
+            $table->integer('drawing')->nullable();
+            $table->integer('revenue')->nullable();
+            $table->integer('expense')->nullable();
+            $table->integer('created_by')->nullable();
+            $table->integer('updated_by')->nullable();
             $table->timestamps();
         });
     }
