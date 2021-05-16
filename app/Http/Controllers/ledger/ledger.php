2b682c4df3d7d6bfa7f1c0af63e15576f24c175d
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-17 00:42:08 +0600

    ledger started to add

--- app/Http/Controllers/ledger/ledger.php
+++ app/Http/Controllers/ledger/ledger.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers\ledger;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
-use App\Model\ledgermodel;
+use App\Model\ledgermodels;
 class ledger extends Controller
 {
     public function viewer()
         {
             //to use array function
             //$data['countLogo'] = Logo::count();
-            $data['alldata']=ledgermodel::where('created_by',Auth::user()->id)->get();
+            $data['alldata']=ledgermodels::where('created_by',Auth::user()->id)->get();
             
             //dd($data);
             return view('frontend.ledgermodel.viewer-ledgermodel',$data);
@@ -32,20 +32,20 @@ class ledger extends Controller
     {
     	
 
-    	$data = new ledgermodel();
+    	$data = new ledgermodels();
     	$data->short_title =$request->short_title;
     	$data->long_title =$request->long_title;
     	$data->created_by = Auth::user()->id;
     		
     	$data->save();
-    	return redirect()->route('sliders.view')->with('success','ledgermodel inserted  successfully');
+    	return redirect()->route('ledgermodel.view')->with('success','ledgermodel inserted  successfully');
 
     	
     }
 
     public function edit($id)
     {
-    	$editData=ledgermodel::find($id);
+    	$editData=ledgermodels::find($id);
     	//dd($editData);
     	return view('frontend.ledgermodel.edit-ledgermodel',compact('editData'));
     }
@@ -53,7 +53,7 @@ class ledger extends Controller
 
     public function update(Request $request,$id)
     {
-    	$data = ledgermodel::find($id);
+    	$data = ledgermodels::find($id);
     	$data->short_title =$request->short_title;
     	$data->long_title =$request->long_title;
     	$data->created_by = Auth::user()->id;
@@ -67,19 +67,14 @@ class ledger extends Controller
     		$data['image']=$filename;
     	}
     	$data->save();
-    	return redirect()->route('sliders.view')->with('success','data updated successfully');
+    	return redirect()->route('ledgermodel.view')->with('success','data updated successfully');
     	
     }
 
     public function delete($id)
     {
-    	$ledgermodel = ledgermodel::find($id);
-    	if (file_exists('public/upload/user_images'.$ledgermodel->image) AND ! empty($ledgermodel->image)) {
-    		
-
-    		unlink('public/upload/user_images'.$ledgermodel->image);
-    	}
+    	$ledgermodel = ledgermodels::find($id);
     	$ledgermodel->delete();
-    	return redirect()->route('sliders.view')->with('success','ledgermodel deleted successfully');
+    	return redirect()->route('ledgermodel.view')->with('success','ledgermodel deleted successfully');
     }
 }
