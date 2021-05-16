f5fa981701731657e8307af7ec465e71c6d9454c
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-17 02:33:43 +0600

    ledger started to add

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,45 +15,34 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-    @guest
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }} <span class="sr-only">(current)</span></a>
-      </li>
-      @if (Route::has('register'))
-      <li class="nav-item">
-        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-      </li>
-      @endif
-      @else
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">{{ Auth::user()->name }}</a>
-          <a class="dropdown-item" href="#">{{ Auth::user()->email }}</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-              document.getElementById('logout-form').submit();"> {{ __('Logout') }} </a>
+        @guest
+          <li class="nav-item active">
+            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }} <span class="sr-only">(current)</span></a>
+          </li>
+          @if (Route::has('register'))
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+          </li>
+          @endif
+          @else
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">{{ Auth::user()->name }}</a>
+              <a class="dropdown-item" href="#">{{ Auth::user()->email }}</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();"> {{ __('Logout') }} </a>
 
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-              </form>
-              @endguest  
-        </div>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href=" {{route('ledgermodel.viewer')}} ">View Ledger</a>
-      </li>
-    </ul>
-     <li class="nav-item active">
-        <a class="nav-link" href=" {{route('ledgermodel.add')}} ">Add Ledger</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                  </form>
+                  @endguest  
+            </div>
+          </li>
+      </ul>
   </div>
 </nav>
 
@@ -61,11 +50,11 @@
 
 <div class="jumbotron">
   <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <p class="lead">To use the ledger facility clik the button below</p>
   <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p>experiance a new better smooth ledger management system</p>
   <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    <a class="btn btn-primary btn-lg" href="{{route('ledgermodel.viewer')}}" role="button">Ledger</a>
   </p>
 </div>
 
