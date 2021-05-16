7c5c868f6232766769471ac557f09ad3c88fbf17
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-17 02:28:24 +0600

    ledger started to add

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,21 +59,14 @@
 
 
 
-<div class="card text-center">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    @if (session('status'))
-    <a href="#" class="btn btn-primary">{{ session('status') }}</a>
-  
-     @endif
-  </div>
-  <div class="card-footer text-muted">
-   Hey You are logged in!
-  </div>
+<div class="jumbotron">
+  <h1 class="display-4">Hello, world!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
 </div>
 
 
