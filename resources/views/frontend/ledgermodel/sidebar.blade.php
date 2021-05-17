d29e9549c01f87e39fe30eda017ea9047c04ef07
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-17 15:58:37 +0600

    ledger started to add

--- resources/views/frontend/ledgermodel/sidebar.blade.php
+++ resources/views/frontend/ledgermodel/sidebar.blade.php
@@ -3,9 +3,7 @@
  <!-- Sidebar Menu -->
   <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-         <!-- @if(Auth::user()->usertype=='Admin') -->
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+        
           
           <li class="nav-item has-treeview ">
             
