01c921d948b28f7a3ff1a7394f39f0691ac9b17e
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-17 15:40:01 +0600

    ledger started to add

--- resources/views/frontend/ledgermodel/master.blade.php
+++ resources/views/frontend/ledgermodel/master.blade.php
@@ -164,7 +164,7 @@
                     </li>
 
                     <li class="nav-item">
-                      <a href="{{route('add_user')}}" class="nav-link ">
+                      <a href="" class="nav-link ">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Add user</p>
                       </a>
