22bf7b2e637e3bc8ca59862537aa62a0a3bcbe92
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-17 19:47:15 +0600

    ledger started to add

--- routes/web.php
+++ routes/web.php
@@ -21,18 +21,18 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'Frontend\frontendcontroller@index')->name('home');
 
+Route::group(['middleware'=>'test'],function({
 
+		Route::prefix('ledger')->group(function(){
 
+		//Route::get('/view', 'ledger\LedgerController@view')->name('sliders.view');
+		Route::get('/master', 'ledger\ledger@master')->name('ledgermodel.master');
+		Route::get('/viewer', 'ledger\ledger@viewer')->name('ledgermodel.viewer');
+		Route::get('/add', 'ledger\ledger@add')->name('ledgermodel.add');
+		Route::post('/store', 'ledger\ledger@store')->name('ledgermodel.store');
+		Route::get('/edit/{id?}', 'ledger\ledger@edit')->name('ledgermodel.edit');
+		Route::post('/upadate/{id?}', 'ledger\ledger@update')->name('ledgermodel.update');
+		Route::get('/delete/{id?}', 'ledger\ledger@delete')->name('ledgermodel.delete');
+	});
 
-Route::prefix('ledger')->group(function(){
-
-	//Route::get('/view', 'ledger\LedgerController@view')->name('sliders.view');
-	Route::get('/master', 'ledger\ledger@master')->name('ledgermodel.master')->middleware('test');
-	Route::get('/viewer', 'ledger\ledger@viewer')->name('ledgermodel.viewer')->middleware('test');
-	Route::get('/add', 'ledger\ledger@add')->name('ledgermodel.add')->middleware('test');
-	Route::post('/store', 'ledger\ledger@store')->name('ledgermodel.store')->middleware('test');
-	Route::get('/edit/{id?}', 'ledger\ledger@edit')->name('ledgermodel.edit')->middleware('test');
-	Route::post('/upadate/{id?}', 'ledger\ledger@update')->name('ledgermodel.update')->middleware('test');
-	Route::get('/delete/{id?}', 'ledger\ledger@delete')->name('ledgermodel.delete')->middleware('test');
 });
-
