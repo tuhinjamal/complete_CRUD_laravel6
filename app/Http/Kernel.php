a0f655280c088ae68597026ad52e3a700865b284
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-17 20:02:53 +0600

    ledger started to add

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'test' =>\App\Http\Middleware\Test::class,
+        'ledger' =>\App\Http\Middleware\Ledger::class,
     ];
 
     /**
