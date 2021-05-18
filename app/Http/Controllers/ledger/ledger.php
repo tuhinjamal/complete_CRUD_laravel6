9be0375209c15ad205133f09cbe44929c6cf9ae1
tuhinjamal <55732076+tuhinjamal@users.noreply.github.com>
2021-05-18 23:32:23 +0600

    ledger started to add

--- app/Http/Controllers/ledger/ledger.php
+++ app/Http/Controllers/ledger/ledger.php
@@ -24,7 +24,7 @@ class ledger extends Controller
             $sum2['sum2']=ledgermodels::where('created_by',Auth::user()->id)->sum('note_payble','account_payble','capital','drawing','revenue','expense');
             
             //dd($sum,$sum2);
-            return view('frontend.ledgermodel.viewer-ledgermodel',$data,$sum,$sum2);
+            return view('frontend.ledgermodel.viewer-ledgermodel',[$data,$sum,$sum2]);
             //to use compact function
             /*$alldata = User::all();
             dd($alldata->toArray());
